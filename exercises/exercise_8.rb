cc3499e19756f0bb293e3484116f31de77fc0737
Matthew Wardle <mwardle.22@gmail.com>
2019-12-10 01:47:53 +0000

    Complete exercise 8

---
+++ exercises/exercise_8.rb
@@ -0,0 +1,17 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+require_relative './exercise_6'
+require_relative './exercise_7'
+
+puts "Exercise 8"
+puts "----------"
+
+# Your code goes here ...
+
+norm =@store1.employees.create(first_name: "Norm", last_name: "Mac", hourly_rate: 200)
+
+puts norm.password
--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,13 @@ class Employee < ActiveRecord::Base
   validates :last_name, presence: true
   validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
   validates :store_id, presence: true
+
+  before_save :generate_password
+
+  private
+
+  def generate_password
+    self.password = ('a'..'z').to_a.shuffle[0,8].join
+  end
+
 end
