d90f71f51d47a9527502e2821e09d100b3612c4f
Matthew Wardle <mwardle.22@gmail.com>
2019-12-10 01:59:24 +0000

    Complete exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -4,9 +4,25 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { greater_than: 0 }
   validate :must_carry_clothes
 
+  before_destroy :check_employees
+
+  private
+
   def must_carry_clothes
     if mens_apparel == false && womens_apparel == false
       errors.add(:store, "must carry some kind of clothes")
     end
   end
+
+  def check_employees
+    begin
+      if self.employees.size() > 0
+        throw :abort
+      end
+    rescue
+      puts "Cannot delete store with current employees"
+      return false
+    end
+  end
+
 end
