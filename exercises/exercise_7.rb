f0d7705ebd49987368e9279f37db89fccf396cd7
Matthew Wardle <mwardle.22@gmail.com>
2019-12-10 01:07:04 +0000

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter your store name."
+store_name = gets.chomp
+new_store = Store.create(name: store_name, annual_revenue: 0, mens_apparel: false, womens_apparel: false)
+
+puts new_store.errors.messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+  validate :must_carry_clothes
+
+  def must_carry_clothes
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:store, "must carry some kind of clothes")
+    end
+  end
 end
